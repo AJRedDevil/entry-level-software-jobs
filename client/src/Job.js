837cd84ce4d848b4b3da58e67ba07665597cd9b7
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-12-22 18:29:56 +0545

    feat(jobs): Apply css to job

--- client/src/Job.js
+++ client/src/Job.js
@@ -4,12 +4,14 @@ import {Paper, Typography} from '@material-ui/core';
 export default function Job({job, onClick}) {
   return (
     <Paper onClick={onClick} className="job">
-      <div>
-        <Typography variant="h6">{job.title}</Typography>
-        <Typography variant="h5">{job.company}</Typography>
-        <Typography>{job.location}</Typography>
+      <div className="flex-align-md">
+        <div className="job-title-location ">
+          <Typography variant="h6">{job.title}</Typography>
+          <Typography variant="h5">{job.company}</Typography>
+          <Typography>{job.location}</Typography>
+        </div>
       </div>
-      <div>
+      <div className="flex-align-md">
         <Typography>
           {job.created_at
             .split(' ')
