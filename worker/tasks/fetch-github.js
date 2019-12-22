5a035744efb19a5115b6781841776994bded18dd
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-12-22 15:07:09 +0545

    feat(algo): Add simple filter algorithm

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -7,6 +7,16 @@ const setAsync = promisify(client.set).bind(client);
 
 const baseURL = 'https://jobs.github.com/positions.json';
 
+const filterAlgo = ({title}) => {
+  const jobTitle = title.toLowerCase();
+  return !(
+    jobTitle.includes('senior') ||
+    jobTitle.includes('manager') ||
+    jobTitle.includes('sr.') ||
+    jobTitle.includes('architect')
+  );
+};
+
 const fetchGithub = async () => {
   let resultCount = 1,
     onPage = 0;
@@ -22,6 +32,13 @@ const fetchGithub = async () => {
   }
 
   console.log(`got ${allJobs.length} jobs`);
+
+  // filter algo
+  const jrJobs = allJobs.filter(filterAlgo);
+
+  console.log(`filtered down to ${jrJobs.length}`);
+
+  // set in redis
   const successs = await setAsync('github', JSON.stringify(allJobs));
 
   console.log(successs);
