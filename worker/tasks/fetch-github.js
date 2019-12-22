7d4b794cbe01c4cff08bacceb70da711f83c80ed
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-12-22 13:55:11 +0545

    feat(worker): Fetch the available github jobs

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -3,9 +3,20 @@ const fetch = require('node-fetch');
 const baseURL = 'https://jobs.github.com/positions.json';
 
 const fetchGithub = async () => {
-  const res = await fetch(baseURL);
-  const jobs = await res.json();
-  console.log(jobs.length);
+  let resultCount = 1,
+    onPage = 0;
+  const allJobs = [];
+
+  while (resultCount > 0) {
+    const res = await fetch(`${baseURL}?page=${onPage}`);
+    const jobs = await res.json();
+    allJobs.push(...jobs);
+    resultCount = jobs.length;
+    console.log(`got ${resultCount} jobs`);
+    onPage++;
+  }
+
+  console.log(`got ${allJobs.length} jobs`);
 };
 
 fetchGithub();
