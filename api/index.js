7ac6eab08921560accad93eeb9899ded47e68f83
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-12-22 16:24:27 +0545

    feat(api): Enable cors for localhost:3000 client

--- api/index.js
+++ api/index.js
@@ -7,6 +7,15 @@ const getAsync = promisify(client.get).bind(client);
 const app = express();
 const port = 5000;
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.get('/', (req, res) => res.send('Jobs API!'));
 app.get('/jobs', async (req, res) => {
   const jobs = await getAsync('github');
