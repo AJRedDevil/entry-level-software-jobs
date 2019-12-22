8e78d34ecc2783aef626f0f4d1c4c719d6450a34
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-12-22 17:25:33 +0545

    feat(jobs): Add stepper for pagination

--- client/src/Jobs.js
+++ client/src/Jobs.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import {Typography} from '@material-ui/core';
 
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+
 import Job from './Job';
 
 export default function Jobs({jobs}) {
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  };
+
+  console.log(jobs[0]);
+
   return (
     <div className="jobs">
       <Typography variant="h4" component="h1">
@@ -12,6 +29,24 @@ export default function Jobs({jobs}) {
       {jobs.map(job => (
         <Job job={job} />
       ))}
+      <MobileStepper
+        variant="progress"
+        steps={6}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
+            Next
+            <KeyboardArrowRight />
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            <KeyboardArrowLeft />
+            Back
+          </Button>
+        }
+      />
     </div>
   );
 }
