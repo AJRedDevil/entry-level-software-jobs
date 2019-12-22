de892ef6453f20de3d45124fb36578e7e8be0322
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-12-22 16:25:28 +0545

    feat(client): Fetch jobs from api

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,20 @@ import './App.css';
 
 import Jobs from './Jobs';
 
+const JOB_API_URL = 'http://localhost:5000/jobs';
+
 const mockJobs = [
   {title: 'SME 1', company: 'Facebook'},
   {title: 'SME 1', company: 'Google'},
   {title: 'SME 1', company: 'Apple'},
 ];
+
+const fetchJobs = async () => {
+  const res = await fetch(JOB_API_URL);
+  const json = await res.json();
+
+  console.log({json});
+};
 
 function App() {
   return (
