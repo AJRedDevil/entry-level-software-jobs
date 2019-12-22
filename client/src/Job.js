7941e43ad5146a1726c3756273e42be09b3c844d
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-12-22 10:57:03 +0545

    feat(job): Render job title and company

--- client/src/Job.js
+++ client/src/Job.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-export default function Job() {
-  return <div className="job">Single Job</div>;
+export default function Job({job}) {
+  return (
+    <div className="job">
+      {job.title}
+      {job.company}
+    </div>
+  );
 }
