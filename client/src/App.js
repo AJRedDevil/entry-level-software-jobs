6c31053c27e71674fdc2c25fac86b988798354e8
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-12-22 10:40:01 +0545

    feat(jobs): Add Jobs container

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Jobs from './Jobs';
+
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <Jobs />
+    </div>
+  );
 }
 
 export default App;
